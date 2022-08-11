d71ac352588ccc484f3179d40d31096c33928a2a
Patrick Thomson <patrickt@github.com>
2022-08-11 10:01:17 -0400

    Attempts at getting completion.

--- codespaces.el
+++ codespaces.el
@@ -5,7 +5,7 @@
 ;; Author: Patrick Thomson <patrickt@github.com>
 ;; URL: https://github.com/patrickt/codespaces.el
 ;; Package-Version: 0.1
-;; Package-Requires: ((emacs "28.1"))
+;; Package-Requires: ((emacs "28.1") (dash "2.19"))
 ;; Keywords: tramp
 ;; Created: 2022-08-11
 
@@ -30,9 +30,53 @@
 ;; via TRAMP in Emacs. It also will provide a completing-read interface
 ;; to select codespaces.
 
-;; This package works by registering  as
+;; This package works by registering a new "ghcs" method in tramp-methods.
 
 ;;; Code:
+
+(defun codespaces-setup ()
+  "Set up the ghcs tramp-method. Should be called after requiring this package."
+  (interactive)
+  (unless (executable-find "gh")
+    (error "Could not find `gh' program in your PATH."))
+  (unless (and (fboundp 'json-available-p) (json-available-p))
+    (error "Emacs JSON support not available; your Emacs is too old."))
+  (let ((ghcs (assoc "ghcs" tramp-methods))
+        (ghcs-methods '((tramp-login-program "gh")
+                        (tramp-login-args (("codespace") ("ssh") ("-c") ("%h")))
+                        (tramp-remote-shell "/bin/sh")
+                        (tramp-remote-shell-login ("-l"))
+                        (tramp-remote-shell-args ("-c")))))
+    ;; just for debugging the methods
+    (if ghcs (setcdr ghcs ghcs-methods)
+      (push (cons "ghcs" ghcs-methods) tramp-methods))))
+
+(defun codespaces--get-codespaces ()
+  (letrec
+      ((gh-invocation "gh codespace list --json name,displayName,repository,state,gitStatus,lastUsedAt")
+       (codespace-json (shell-command-to-string gh-invocation)))
+    (json-parse-string codespace-json)))
+
+(defun codespaces--munge (json)
+  (-map (lambda (n) (list (gethash "name" n) (gethash "repository" n))) json))
+
+(defun codespaces--annotate (s)
+  (let ((item (assoc s minibuffer-completion-table)))
+    (when item (concat "  -- " (nth 1 item)))))
+
+(defun codespaces--complete ()
+  (interactive)
+  (letrec
+      ((json (codespaces--get-codespaces))
+       (valid-names (codespaces--munge json)))
+    (let ((completion-extra-properties '(:annotation-function codespaces--annotate)))
+      (completing-read "Please select a codespace: " valid-names))))
+
+(defun codespaces-connect ()
+  "Connect to a running codespace."
+  (interactive)
+  (let (cs (codespaces--complete))
+    (find-file (format "/ghcs:patrickt-git:/workspaces"))))
 
 (provide 'codespaces)
 
